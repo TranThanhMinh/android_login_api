e2b1e1c03b0a14d30473ad30c7da85bbd0f5b06c
TranThanhMinh <minhtt.cdcntt@gmail.com>
2015-07-19 11:00:09 +0700

    android_login_api
    android_login_api

---
+++ android_login_api/include/config.php
@@ -0,0 +1,10 @@
+<?php
+
+/**
+ * Database config variables
+ */
+define("DB_HOST", "mysql3.000webhost.com");//define("DB_HOST", "localhost");
+define("DB_USER", "a7787746_android");//define("DB_USER", "root");
+define("DB_PASSWORD", "hieutv123");//define("DB_PASSWORD", "");
+define("DB_DATABASE", "a7787746_android");//define("DB_DATABASE", "android_api");
+?>
